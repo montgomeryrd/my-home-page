aa6243ce06e466a604f20991d280d9491b7df25d
montgomeryrd <montgomery_rd@yahoo.com>
2020-06-15 22:02:26 -0700

    working on sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
 import React from 'react';
+import Sidebar from './components/sidebar/Sidebar';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component() {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // Sidebar state
+      sidebartitle : "",
+      sidebarlink : "",
+      sidebarlist : [],
+    }
+    // Sidebar handles
+    this.handleSidebarTitleChange=this.handleSidebarTitleChange.bind(this);
+    this.handleSidebarLinkChange=this.handleSidebarLinkChange.bind(this);
+    this.handleSidebarSubmit=this.handleSidebarSubmit.bind(this);
+  }
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('landingdata');
+      this.setState({...JSON.parse(json)});
+    } catch (error) {};
+  }
+  componentDidUpdate(prevProps, prevState) {  
+    const json = JSON.stringify(this.state);
+    localStorage.setItem('landingdata', json);
+  }
+  // Sidebar functions
+  handleSidebarTitleChange = (e) => {
+    this.setState({sidebartitle : e.target.value});
+  }
+  handleSidebarLinkChange = (e) => {
+    this.setState({sidebarlink : e.target.value});
+  }
+  handleSidebarSubmit = (e) => {
+    e.preventDefault();
+    this.sidebar.add(this.state.sidebartitle, this.state.sidebarlink);
+  }
+  sidebar = {
+    add(title, link) {
+      const song = {title, link};
+      this.setState({sidebarlist : [...this.state.sidebarlist, song]});
+    },
+    remove(title) {
+      const sidebarlist = this.state.sidebarlist.filter(song => song.title !== title);
+      this.setState({sidebarlist : sidebarlist});
+    },
+  }
 
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <div className="sidebar-container">
+          <Sidebar 
+            sidebartitle = {this.sidebartitle}
+            sidebarlink = {this.sidebarlink}
+            sidebarlist = {this.sidebarlist}
+            handleSidebarTitleChange = {this.handleSidebarTitleChange}
+            handleSidebarLinkChange = {this.handleSidebarLinkChange}
+            handleSidebarSubmit = {this.handleSidebarSubmit}
+            sidebar = {this.sidebar}         
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SidebarForm from './SidebarForm';
 
-const Coding = (props) => {
-    return (
+const Sidebar = (props) => {
+    const [form, setForm] = useState(false);
+    const toggle = () => setForm(!form);
+    let playingtitle = "", playinglink = "";
+
+    function song(title, content){
+        playingtitle = title;
+        playinglink = content;
+    };
+
+    const links = props.sidebarlist.length ? (
+        props.sidebarlist.map((link, index) => {
+            return (
+                <div className="links-container" unselectable="on" key={index}>
+                    <h2>{link.title}</h2>
+                    <p onClick={() => {song(link.title, link.content)}}>play</p>
+                </div>
+            )
+        })
+    ) : (
         <div>
 
         </div>
+    );
+
+    return (
+        <div className="sidebar-content">
+            <h1>CURRENT TUNES</h1>
+            <h6 onClick={toggle}>add link</h6>
+            {form ?
+                <div>
+                    <SidebarForm 
+                        sidebartitle = {props.sidebartitle}
+                        sidebarlink = {props.sidebarlink}
+                        sidebarlist = {props.sidebarlist}
+                        handleSidebarChange = {props.handleSidebarChange}
+                        handleSidebarSubmit = {props.handleSidebarSubmit}
+                    />
+                </div>
+            :
+                <div>
+                    {links}
+                </div>
+            }
+            <div>
+            <iframe 
+                title={playingtitle}
+                width="560" 
+                height="315" 
+                src={playinglink}
+                frameborder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen
+            />
+            </div>
+        </div>
     )
 }
-export default Coding;
+export default Sidebar;
